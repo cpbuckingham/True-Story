e488814509c92a9a40928c62b08bb925f7b563a2
Cameron <cameron.p.buckingham@gmail.com>
2015-08-29 09:08:54 -0700

    app.js updated with corrected Member + ending

--- app/public/javascripts/app.js
+++ app/public/javascripts/app.js
@@ -44,7 +44,7 @@ InstructorApp = {
   renderSummary: function (data) {
     var $summary = $("[data-container=student-summary]");
     var ending = data.length == 1 ? '' : 's';
-    $summary.html(data.length + ' Developer' + ending);
+    $summary.html(data.length + ' Member' + ending);
   },
 
   renderDetails: function (data) {
