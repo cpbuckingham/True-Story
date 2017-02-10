fed809f2668aa0278d14234754cc4c1e4a960e8b
Cameron Buckingham <cpbuckingham@Camerons-MacBook-Air.local>
2017-02-10 07:47:49 -0600

    new rsa

--- lib/pub_sub.rb
+++ lib/pub_sub.rb
@@ -1,6 +1,6 @@
 class PubSub
   def initialize
-    Pusher.url =  "http://ad19028119cbf87369e4:491d4c83af1dc4cb96eb@api.pusherapp.com/apps/136577"
+    Pusher.url = ENV['PUSHER_KEY']
   end
 
   def publish(channel, event, data)
